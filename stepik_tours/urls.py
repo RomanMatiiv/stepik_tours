89a58221610bed9c93479340061d9a4febb2e283
matiiv <matiiv.r.a@gmail.com>
2022-01-18 01:06:17 +0300

    add view and handler

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from tours.views import departure_view
+from tours.views import main_view
+from tours.views import tour_view
+from tours.views import custom_handler400
+from tours.views import custom_handler403
+from tours.views import custom_handler404
+from tours.views import custom_handler500
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', main_view),
+    path('departure/<str:departure>/', departure_view),
+    path('tour/<int:id>/', tour_view),
 ]
+
+handler400 = custom_handler400
+handler403 = custom_handler403
+handler404 = custom_handler404
+handler500 = custom_handler500
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseNotFound
+from django.http import HttpResponseForbidden
+from django.http import HttpResponseServerError
 
-# Create your views here.
+
+def main_view(request):
+    return render(request, 'index.html')
+
+
+def departure_view(request, departure: str):
+    return render(request, 'departure.html')
+
+
+def tour_view(request, id: int):
+    return render(request, 'tour.html')
+
+
+# Handlers
+def custom_handler400(request, exception):
+    # Call when SuspiciousOperation raised
+    return HttpResponseBadRequest('Неверный запрос!')
+
+
+def custom_handler403(request, exception):
+    # Call when PermissionDenied raised
+    return HttpResponseForbidden('Доступ запрещен!')
+
+
+def custom_handler404(request, exception):
+    # Call when Http404 raised
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    # Call when raised some python exception
+    return HttpResponseServerError('Ошибка сервера!')
