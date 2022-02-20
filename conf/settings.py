35e664ab38e92d5b8f1e36909222acce416982dc
matiiv <matiiv.r.a@gmail.com>
2022-02-21 00:06:36 +0300

    rename stepik_tours to conf dir

--- conf/settings.py
+++ conf/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stepik_tours.urls'
+ROOT_URLCONF = 'conf.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stepik_tours.wsgi.application'
+WSGI_APPLICATION = 'conf.wsgi.application'
 
 
 # Database
